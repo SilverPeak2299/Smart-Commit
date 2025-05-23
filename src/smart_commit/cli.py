e8333d1079e0dc92074d7689f40836eaca6e4e58
SilverPeak2299 <dannydickerson03@gmail.com>
2025-05-23 16:09:57 +1000

    feat: Implement AI commit messages

--- src/smart_commit/cli.py
+++ src/smart_commit/cli.py
@@ -1,6 +1,8 @@
 import keyring
 import click
 import subprocess
+from google import genai
+from google.genai import types
 
 def get_git_dif():
     try:
@@ -9,8 +11,27 @@ def get_git_dif():
     except subprocess.CalledProcessError:
         return ""
 
-##test comment
 
+def get_commit_msg(git_diff: str, API_KEY):
+    client = genai.Client(api_key=API_KEY)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash-preview-05-20",
+        config=types.GenerateContentConfig(
+            system_instruction="Generate a conventional commit message from the provided git diff. Use modern conversion and styling. Keep it under 50 characters, present tense, no period."),
+        contents= git_diff
+    )
+    
+    return response.candidates[0].content.parts[0].text
+    
+    
+def commit_with_message(message):
+    try:
+        result = subprocess.run(
+            ['git', 'commit', '-m', message],
+        )
+        return True, result.stdout
+    except subprocess.CalledProcessError as e:
+        return False, e.stderr
 
 @click.command()
 def main():
@@ -22,6 +43,33 @@ def main():
     
     git_diff = get_git_dif()
     
-    print(git_diff)
-        
+    commit_msg = get_commit_msg(git_diff, API_KEY)
+    
+    print(f"Generated Commit Msg: \n{commit_msg}")
+    
+    while True:
+            choice = input("(c)ommit, (e)dit, (r)egenerate, or (q)uit:\n").lower().strip()
+            
+            if choice == 'c':
+                success, output = commit_with_message(commit_msg)
+                if success:
+                    print("Committed successfully!")
+                else:
+                    print(f"❌ Commit failed: {output}")
+                break
+                
+            elif choice == 'e':
+                commit_msg = input(f"Edit message: {commit_msg}\n> ") or commit_msg
+                
+            elif choice == 'r':
+                print("Regenerating...")
+                commit_msg = get_commit_msg(git_diff, API_KEY)
+                print(f"New message: {commit_msg}")
+                
+            elif choice == 'q':
+                print("Cancelled.")
+                break
+            else:
+                print("Please choose c, e, r, or q")
+    
     
